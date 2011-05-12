2dbe0f3235e183276c1aaa7e44b6a222ecf4ae20
Michal Orman <michal.orman@gmail.com>
2011-05-12 21:13:05 +0200

    Added yasnippet.

--- init.el
+++ init.el
@@ -94,5 +94,12 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/haml-mode")
 (require 'haml-mode)
 
+;; yasnippet
+
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+
 
 ;;; init.el ends here
